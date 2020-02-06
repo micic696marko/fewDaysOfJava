36413070480f7a9adeddd9ec5483cf8acf60cded
micic696marko <60735559+micic696marko@users.noreply.github.com>
2020-02-06 16:28:07 +0100

    initial commit

---
+++ FirstProgram/src/com/markomicic/DemoTest1.java
@@ -0,0 +1,20 @@
+package com.markomicic;
+
+import java.util.Scanner;
+
+public class DemoTest1 {
+
+    public static void main(String[] args) {
+	System.out.println("Enter your name");
+
+	Scanner scanner = new Scanner(System.in);
+	
+	int i = 0;
+	
+	do {
+	    System.out.println("i is: " + i);
+	} while( i < 10);
+	scanner.close();
+    }
+
+}
